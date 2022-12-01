32829ca6a69a8666608090df133d72a5bd850bd1
Ella Lopperi <ella.lopperi@gmail.com>
2022-12-01 22:37:48 +0200

    Refactored

--- Day 1/calories.py
+++ Day 1/calories.py
@@ -1,19 +1,12 @@
-with open('input.txt') as f:
+with open('Day 1/input.txt') as f:
     contents = f.read()
-    #print(contents)
     contentsSplit = contents.split("\n\n")
-    #print(contentsSplit)
 
-#This processes e.g. 2642\n11252\n3981\n7926\n9800
-def parseElf(chunk):
-  return sum(list(map(int, chunk.split("\n"))))
+#This processes e.g. 2642\n11252\n3981\n7926\n9800 into list of summed calories
+parseElf = lambda chunk : sum(list(map(int, chunk.split("\n"))))
 
 elfCalories = list(map(parseElf, (contentsSplit)))
 
-elfCalories.sort(reverse=True)
+elfCaloriesSorted = sorted(elfCalories, reverse=True)
 
-topOneElfCalories = elfCalories[0]
-topThreeElfCalories = elfCalories[:3]
-sumTopElfCalories = sum(topThreeElfCalories)
-
-print("Answer 1:" + str(topOneElfCalories),"\nAnswer 2:" + str(sumTopElfCalories))
+print("Answer 1:" + str(elfCaloriesSorted[0]),"\nAnswer 2:" + str(sum(elfCaloriesSorted[:3])))
